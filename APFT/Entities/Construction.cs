766e04dfaf2adb7dc1dbf3b815fc5864a270d05b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-05-28 19:46:57 +0100

    Construction class: add future-needed methods and attributes

--- APFT/Entities/Construction.cs
+++ APFT/Entities/Construction.cs
@@ -8,34 +8,65 @@ public class Construction
 {
     public int Id
     {
-        get;
+        get; set;
     }
+
     public string Location
     {
-        get;
+        get; set;
     }
+
     public DateTime StartDate
     {
-        get;
+        get; set;
     }
-    public DateTime EndDate
+
+    public DateTime? EndDate
     {
-        get;
+        get; set;
     }
+
     public int CustomerNif
     {
-        get;
+        get; set;
     }
-    public string StartDateString => StartDate.ToString().Split(" ")[0];
-    public string EndDateString => EndDate.ToString().Split(" ")[0];
 
-    public Construction(int id, string location, DateTime startDate, DateTime endDate, int customerNif)
+    public string StartDateString => StartDate.ToString().Split(" ")[0];
+    public string EndDateString => EndDate == null ? EndDate.ToString().Split(" ")[0] : string.Empty;
+
+    public Construction(int id, string location, DateTime startDate, DateTime? endDate, int customerNif)
     {
         Id = id;
         Location = location;
         StartDate = startDate;
         EndDate = endDate;
         CustomerNif = customerNif;
+    }
+
+    public static async Task<ObservableCollection<Construction>> GetConstructionsAsync()
+    {
+        var constructions = new ObservableCollection<Construction>();
+        var localSettings = ApplicationData.Current.LocalSettings;
+
+        await using var cn = new SqlConnection(localSettings.Values["SQLConnectionString"].ToString());
+
+        await cn.OpenAsync();
+        var cmd = new SqlCommand("SELECT * FROM EMPRESA_CONSTRUCAO.OBRA", cn);
+
+        var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            constructions.Add(new Construction(
+                reader.GetInt32(0),
+                reader.GetString(1),
+                reader.GetDateTime(2),
+                await reader.IsDBNullAsync(3) ? null : reader.GetDateTime(3),
+                reader.GetInt32(4)
+            ));
+        }
+
+        return constructions;
     }
 
     public static async Task<ObservableCollection<Construction>> GetConstructionsByCustomerNifAsync(int customerNif)
@@ -56,7 +87,34 @@ public class Construction
                 reader.GetInt32(0),
                 reader.GetString(1),
                 reader.GetDateTime(2),
-                reader.GetDateTime(3),
+                await reader.IsDBNullAsync(3) ? null : reader.GetDateTime(3),
+                reader.GetInt32(4)
+            ));
+        }
+
+        return constructions;
+    }
+
+    public static async Task<ObservableCollection<Construction>> GetConstructionsByDateAsync(string startDateString, string endDateString)
+    {
+        var constructions = new ObservableCollection<Construction>();
+        var localSettings = ApplicationData.Current.LocalSettings;
+
+        await using var cn = new SqlConnection(localSettings.Values["SQLConnectionString"].ToString());
+
+        await cn.OpenAsync();
+
+        var cmd = new SqlCommand("SELECT * FROM getObraByDate('" + startDateString + "', '" + endDateString + "')", cn);
+
+        var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            constructions.Add(new Construction(
+                reader.GetInt32(0),
+                reader.GetString(1),
+                reader.GetDateTime(2),
+                await reader.IsDBNullAsync(3) ? null : reader.GetDateTime(3),
                 reader.GetInt32(4)
             ));
         }
