21dad7012ca792a53be5492f4033a4d84a9101da
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-05-29 14:11:27 +0100

    Employee class: create future-needed methods

--- APFT/Entities/Employee.cs
+++ APFT/Entities/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Collections.ObjectModel;
 using Windows.Storage;
+using Microsoft.IdentityModel.Tokens;
 
 namespace APFT.Entities;
 
@@ -8,55 +9,56 @@ public class Employee
 {
     public int Nif
     {
-        get; private set;
+        get; set;
     }
 
     public string FirstName
     {
-        get; private set;
+        get; set;
     }
 
     public string LastName
     {
-        get; private set;
+        get; set;
     }
 
     public string Email
     {
-        get; private set;
+        get; set;
     }
 
     public int Phone
     {
-        get; private set;
+        get; set;
     }
 
     public string? Address
     {
-        get; private set;
+        get; set;
     }
 
     public string? Gender
     {
-        get; private set;
+        get; set;
     }
 
     public DateTime? BirthDate
     {
-        get; private set;
+        get; set;
     }
 
     public decimal Salary
     {
-        get; private set;
+        get; set;
     }
 
     public int DepartmentId
     {
-        get; private set;
+        get; set;
     }
 
     public string Name => FirstName + " " + LastName;
+    public string BirthDateString => BirthDate != null ? BirthDate.ToString().Split(" ")[0] : string.Empty;
 
     public Employee(int nif, string firstName, string lastName, string email, int phone, string address, string gender, DateTime birthDate, decimal salary, int departmentId)
     {
@@ -109,8 +111,59 @@ public class Employee
                 reader.GetString(2),
                 reader.GetString(3),
                 reader.GetInt32(4),
-                await reader.IsDBNullAsync(5) ? "" : reader.GetString(5),
-                await reader.IsDBNullAsync(6) ? "" : reader.GetString(6),
+                await reader.IsDBNullAsync(5) ? string.Empty : reader.GetString(5),
+                await reader.IsDBNullAsync(6) ? string.Empty : reader.GetString(6),
+                await reader.IsDBNullAsync(7) ? new DateTime(1970, 1, 1) : reader.GetDateTime(7),
+                reader.GetDecimal(8),
+                reader.GetInt32(9))
+            );
+        }
+
+        return employees;
+    }
+
+    public static async Task<ObservableCollection<GroupInfoList>> GetEmployeesFilteredGroupedAsync(string? gender, string birthDateString, decimal salary)
+    {
+        // Grab Contact objects from pre-existing list (list is returned from function GetContactsAsync())
+        var query = from item in await GetEmployeesFilteredAsync(gender, birthDateString, salary)
+
+            // Group the items returned from the query, sort and select the ones you want to keep
+            group item by item.FirstName[..1].ToUpper() into g
+            orderby g.Key
+
+            // GroupInfoList is a simple custom class that has an IEnumerable type attribute, and
+            // a key attribute. The IGrouping-typed variable g now holds the Contact objects,
+            // and these objects will be used to create a new GroupInfoList object.
+            select new GroupInfoList(g) { Key = g.Key };
+
+        return new ObservableCollection<GroupInfoList>(query);
+    }
+
+    public static async Task<ObservableCollection<Employee>> GetEmployeesFilteredAsync(string? gender, string birthDateString, decimal salary)
+    {
+        var employees = new ObservableCollection<Employee>();
+        var localSettings = ApplicationData.Current.LocalSettings;
+
+        await using var cn = new SqlConnection(localSettings.Values["SQLConnectionString"].ToString());
+        
+        await cn.OpenAsync();
+        var cmd = new SqlCommand("SELECT * FROM getEmpregadoBySexBirthSalary(" + 
+                                 (string.IsNullOrEmpty(gender) ? "null" : "'" + gender + "'") + ", " +
+                                 (string.IsNullOrEmpty(birthDateString) ? "null" : "'" + birthDateString + "'") + ", " + 
+                                 salary + ")", cn);
+
+        var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            employees.Add(new Employee(
+                reader.GetInt32(0),
+                reader.GetString(1),
+                reader.GetString(2),
+                reader.GetString(3),
+                reader.GetInt32(4),
+                await reader.IsDBNullAsync(5) ? string.Empty : reader.GetString(5),
+                await reader.IsDBNullAsync(6) ? string.Empty : reader.GetString(6),
                 await reader.IsDBNullAsync(7) ? new DateTime(1970, 1, 1) : reader.GetDateTime(7),
                 reader.GetDecimal(8),
                 reader.GetInt32(9))
