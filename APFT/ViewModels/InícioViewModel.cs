ca7654d0fab288fe058b8a153336ce7b1c0a065b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-06-06 05:02:11 +0100

    Partially working rows per table statistics

--- APFT/ViewModels/InícioViewModel.cs
+++ APFT/ViewModels/InícioViewModel.cs
@@ -12,6 +12,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using ABI.Microsoft.UI.Xaml.Data;
+using String = System.String;
 
 namespace APFT.ViewModels;
 
@@ -31,29 +32,72 @@ public partial class InícioViewModel : ObservableObject, INotifyPropertyChanged
 
     private async void FetchData()
     {
-        var employeeData = await Employee.GetHoursOfAllEmployees();
-
-        var hoursData = employeeData.Select(employee => employee.Hours.HasValue ? (double)employee.Hours.Value : 0).ToArray();
         var i = 0;
 
-        Series = hoursData.AsLiveChartsPieSeries((value, series) =>
+        var employeeData = await Employee.GetHoursOfAllEmployees();
+        var hoursData = employeeData.Select(employee => employee.Hours.HasValue ? (double)employee.Hours.Value : 0).ToArray();
+        EmployeeSeries = hoursData.AsLiveChartsPieSeries((value, series) =>
         {
             series.Name = employeeData[i].Name;
             i++;
             series.InnerRadius = 80;
         });
+        
+        var tableData = await Info.GetTableInfo();
+        var tableDataRowNumbers = tableData.Select(table => table.Rows ?? 0).ToArray();
+        RowsSeries = new ISeries[]
+        {
+            new ColumnSeries<long> { Values = tableDataRowNumbers}
+        };
+        RowsXAxes = new Axis[] 
+        {
+            new() 
+            {
+                // Use the labels property for named or static labels 
+                Labels = tableData.Select(table => table.TableName ?? string.Empty).ToArray(), 
+                LabelsRotation = 15
+            }
+        };
     }
 
-    private IEnumerable<ISeries> _series;
-    public IEnumerable<ISeries> Series
+    private IEnumerable<ISeries> _employeeSeries;
+    public IEnumerable<ISeries> EmployeeSeries
     {
-        get => _series;
+        get => _employeeSeries;
         set
         {
-            if (_series != value)
+            if (_employeeSeries != value)
             {
-                _series = value;
-                OnPropertyChanged(nameof(Series));
+                _employeeSeries = value;
+                OnPropertyChanged(nameof(EmployeeSeries));
+            }
+        }
+    }
+
+    private ISeries[] _rowsSeries;
+    public ISeries[] RowsSeries
+    {
+        get => _rowsSeries;
+        set
+        {
+            if (_rowsSeries != value)
+            {
+                _rowsSeries = value;
+                OnPropertyChanged(nameof(RowsSeries));
+            }
+        }
+    }
+
+    private Axis[] _rowsXAxes;
+    public Axis[] RowsXAxes
+    {
+        get => _rowsXAxes;
+        set
+        {
+            if (_rowsXAxes != value)
+            {
+                _rowsXAxes = value;
+                OnPropertyChanged(nameof(RowsXAxes));
             }
         }
     }
